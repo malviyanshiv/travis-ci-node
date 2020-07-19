3a11d4da94db5a0a4f692955e136dcdf2c0bcf3d
Shiv Shankar Singh <malviyanshiv@gmail.com>
2020-07-19 17:05:43 +0530

    [add] increament test with default value

--- test/app.test.js
+++ test/app.test.js
@@ -4,3 +4,8 @@ test("testing increament with both arguments", () => {
     const result = increament(10, 5);
     expect(result).toBe(15);
 });
+
+test("testing increament with default increment value", () => {
+    const result = increament(10);
+    expect(result).toBe(12);
+});
